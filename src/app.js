93edbea46f74fcf5d8378adaa57cd2248088be5d
Sprengzor <otto.spreng@sou.unifeob.edu.br>
2019-05-31 13:20:41 -0300

    adding users and auth

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 
 import authorRoutes from "./routes/AuthorRoutes";
 import bookRoutes from "./routes/BookRoutes";
+import userRoutes from "./routes/UserRoutes";
+import sessionRoutes from "./routes/SessionRoutes";
 
 const app = express();
 const BASE_URI = "/api/v1";
@@ -10,8 +12,10 @@ const BASE_URI = "/api/v1";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(BASE_URI, sessionRoutes);
 app.use(BASE_URI, authorRoutes);
-app.use(BASE_URI,bookRoutes);
+app.use(BASE_URI, bookRoutes);
+app.use(BASE_URI, userRoutes);
 
 app.get("*", (req, res) => {
   res.status(200).send({ messsage: "Welcome to this API" });
